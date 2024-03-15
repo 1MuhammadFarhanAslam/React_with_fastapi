ed1d30603780242028f827ad6587cfa522b7c48c
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-03-15 13:07:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -279,15 +279,15 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # SQLAlchemy models
 Base = declarative_base()
 
-# def React_JWT_Token(data: dict, expires_delta: timedelta):
-#     to_encode = data.copy()
-#     expire = datetime.utcnow() + expires_delta
-#     to_encode.update({"exp": expire})
-#     encoded_jwt = jwt.encode(to_encode, GOOGLE_LOGIN_SECRET_KEY, algorithm=ALGORITHM)
-#     return encoded_jwt
+def React_JWT_Token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, GOOGLE_LOGIN_SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 # Dependency to get the database session
-def get_database():
+def get_database() -> Generator[Session, None, None]:
     # Provide a database session to use within the request
     db = SessionLocal()
     try:
