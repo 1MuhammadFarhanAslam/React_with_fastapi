3f3d3d9ccb7d5eaac991a6d6a6dbdfe6cf5250f0
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-03-15 15:49:03 +0500

    Update react.py

--- routers/react.py
+++ routers/react.py
@@ -110,41 +110,6 @@ async def google_signin(token: React_user_Token, db: Session = Depends(get_datab
         print(e)
         raise HTTPException(status_code=500, detail="Server Error")
 
-
-
-@router.get("/read/{id}", response_model=None, tags=["React"])
-async def read_react_user(
-    id: str,
-    db: Session = Depends(get_database)
-):
-    try:
-        # Query the user based on the role
-        user = db.query(React_User).filter(React_User.id == id).first()
-        
-        logger.info(f"Attempting to retrieve user with id: {id}")
-        print("_____________________User_____________________" , user)
-        
-        if user:
-            user_data = {
-                "id": str(user.id),
-                "created_at": user.created_at,
-                "username": user.username,
-                "email": user.email,
-                "picture": user.picture,
-                "email_verified": user.email_verified,
-                "role": user.role,
-            }
-
-            return user_data
-        else:
-            logger.warning(f"No user found with id: {id}")
-            raise HTTPException(status_code=404, detail="No user found")
-    except ValueError:
-        logger.warning(f"Invalid id: {id}")
-        raise HTTPException(status_code=400, detail="Invalid role")
-    except Exception as e:
-        logger.error(f"Error during user retrieval: {e}")
-        raise HTTPException(status_code=500, detail="Internal Server Error")
     
 
 @router.get("/read", response_model=None, tags=["React"])
@@ -180,4 +145,39 @@ async def jwt_token_react_user(
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
     except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+    
+
+@router.get("/read/{id}", response_model=None, tags=["React"])
+async def read_react_user(
+    id: str,
+    db: Session = Depends(get_database)
+):
+    try:
+        # Query the user based on the role
+        user = db.query(React_User).filter(React_User.id == id).first()
+        
+        logger.info(f"Attempting to retrieve user with id: {id}")
+        print("_____________________User_____________________" , user)
+        
+        if user:
+            user_data = {
+                "id": str(user.id),
+                "created_at": user.created_at,
+                "username": user.username,
+                "email": user.email,
+                "picture": user.picture,
+                "email_verified": user.email_verified,
+                "role": user.role,
+            }
+
+            return user_data
+        else:
+            logger.warning(f"No user found with id: {id}")
+            raise HTTPException(status_code=404, detail="No user found")
+    except ValueError:
+        logger.warning(f"Invalid id: {id}")
+        raise HTTPException(status_code=400, detail="Invalid role")
+    except Exception as e:
+        logger.error(f"Error during user retrieval: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
