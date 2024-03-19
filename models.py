9e15eed991701de6165fdd6dba7a7ab4dac9a58d
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-03-19 17:22:35 +0500

    Updated

--- models.py
+++ models.py
@@ -93,6 +93,9 @@ class Token(BaseModel):
     access_token: str
     token_type: str
 
+class TokenData(BaseModel):
+    username: str | None = None
+
 class AdminCreate(BaseModel):
     username: str
     password: str
