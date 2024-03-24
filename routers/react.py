786d31bc76534d7e6e2508adf739fe9476301d33
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-03-25 01:32:47 +0500

    Update react.py

--- routers/react.py
+++ routers/react.py
@@ -406,7 +406,8 @@ async def email_signup(request: Request, db: Session = Depends(get_database)):
                 "token_type": "bearer"
             }
         
-    except:
+    except Exception as e:
+        print(f"User already exists. Please sign in instead.: {e}")
         raise HTTPException(status_code=400, detail="User already exists. Please sign in instead.")
     
 
