c4532bdce65d6141d35dd6d623c865b353724762
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-04-02 07:35:19 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ initialize_database()
 # Allow CORS for only the React frontend server
 origins = [
     "http://85.239.241.96:3000",  # Your React frontend server's HTTP URL
-    'http://85.239.241.96:8000/'
+    'http://85.239.241.96:8000'
 ]
 
 # Allow CORS for all domains in this example
