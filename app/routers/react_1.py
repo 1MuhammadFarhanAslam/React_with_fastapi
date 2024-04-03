02fd0202dd5ca348602daab0c1b71d5398f42fba
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-04-03 15:17:12 +0500

    Update react_1.py

--- app/routers/react_1.py
+++ app/routers/react_1.py
@@ -156,7 +156,7 @@ LOGIN_PASSWORD = "Opentensor@12345"
 # Define a function to log in the user and get the access token
 def login_user():
     # Make a request to the login endpoint to log in the user
-    login_url = "http://149.11.242.18:14094/login"  # Adjust the URL as needed
+    login_url = "http://149.11.242.18:40337/login"  # Adjust the URL as needed
     login_payload = {
         "username": LOGIN_USERNAME,
         "password": LOGIN_PASSWORD
