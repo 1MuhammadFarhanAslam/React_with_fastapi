e023d5b6e5fb7a5afbb0f278ea923368d3b716fb
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-04-09 02:52:28 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -32,7 +32,9 @@ initialize_database()
 origins = [
     "http://85.239.241.96:3000",  # Your React frontend server's HTTP URL
     'http://85.239.241.96:8000',
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "http://38.80.122.166:40440",
+    "http://79.116.48.205:24942"
 ]
 
 # Allow CORS for all domains in this example
