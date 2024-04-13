6b43bccaaeba626858cce9921255a96f73b19ea5
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-04-13 15:53:18 +0500

    Updated

--- app/main.py
+++ app/main.py
@@ -33,8 +33,8 @@ origins = [
     "http://85.239.241.96:3000",  # Your React frontend server's HTTP URL
     'http://85.239.241.96:8000',
     "http://localhost:3000",
-    "http://93.114.160.254:40308/",
-    "http://93.114.160.254:40716/"
+    "http://93.114.160.254:40308",
+    "http://93.114.160.254:40716"
 ]
 
 # Allow CORS for all domains in this example
--- app/routers/react_1.py
+++ app/routers/react_1.py
@@ -736,8 +736,8 @@ from requests.packages.urllib3.util.retry import Retry
 
 # Define constants for login credentials and URLs
 LOGIN_CREDENTIALS = [
-    {"url": "http://93.114.160.254:40306", "username": "Opentensor@hotmail.com_val1", "password": "Opentensor@12345"},
-    {"url": "http://93.114.160.254:40885", "username": "Opentensor@hotmail.com_val2", "password": "Opentensor@12345"}
+    {"url": "http://93.114.160.254:40308", "username": "Opentensor@hotmail.com_val1", "password": "Opentensor@12345"},
+    {"url": "http://93.114.160.254:40716", "username": "Opentensor@hotmail.com_val2", "password": "Opentensor@12345"}
 ]
 
 def create_session():
