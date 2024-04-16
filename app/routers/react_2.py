cec68c4ffb681c94f486275a9922c8cc609922b5
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-04-16 18:40:11 +0500

    Update react_2.py

--- app/routers/react_2.py
+++ app/routers/react_2.py
@@ -103,7 +103,7 @@ async def text_to_music(request: Request, authorization: Optional[str] = Header(
             
             # Log in the user and get the access token and corresponding URL
             data = {"prompt": prompt}
-            access_token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJCcm9rZW5TdWJuZXRAZ21haWwuY29tIiwiZXhwIjoyMzEzMjUxNTU1fQ.culT9hdkRopQ7cuxShNzmUUC7dGOf-_lvvSv7KOR6dg'
+            access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJCcm9rZW5TdWJuZXRAZ21haWwuY29tIiwiZXhwIjoyMzEzMjUxNTU1fQ.culT9hdkRopQ7cuxShNzmUUC7dGOf-_lvvSv7KOR6dg'
 
             # Construct the TTS URL based on successful login URL
             headers = {
