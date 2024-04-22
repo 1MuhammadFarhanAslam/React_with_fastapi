81ba70d52f97abb4c990a3629c9be5544456db8c
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-04-23 00:27:03 +0500

    Update react_2.py

--- app/routers/react_2.py
+++ app/routers/react_2.py
@@ -248,7 +248,7 @@ async def text_to_music(request: Request, authorization: Optional[str] = Header(
 
         duration = request_data.get("duration")
         print('_______________duration_____________', duration)
-        access_token = os.environ.get("TTM_ACCESS_TOKEN")
+        access_token = authorization
 
         if prompt is None:
             raise HTTPException(status_code=400, detail="Prompt is missing in the request body.")
