9761bfc5e1ceaeac11e0f0ead60ddc0338137815
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-04-22 10:18:42 +0500

    Update react_2.py

--- app/routers/react_2.py
+++ app/routers/react_2.py
@@ -174,7 +174,7 @@ def get_database() -> Generator[Session, None, None]:
 #-------------Working endpoint---------------------- TTM endpoint without auth_token from header, using requests library and time out functionality------------
 # ----------------This endpoint sends requests (using requests library in series manner) to the TTM endpoint and returns the response to the client.
 @router.post("/api/ttm_endpoint")
-async def text_to_music(request: Request, authorization = Header(...)):
+async def text_to_music(request: Request):
     try:
         request_data = await request.json()
         print('_______________request_data_____________', request_data)
@@ -188,14 +188,11 @@ async def text_to_music(request: Request, authorization = Header(...)):
         if prompt is None:
             raise HTTPException(status_code=400, detail="Prompt is missing in the request body.")
 
-        access_token = authorization.split(" ")[1]  # Assuming the header format is "Bearer <token>"
-        
-        print('_______________access_token_____________', access_token)
         try:
             data = {"prompt": prompt, "duration": duration}
             headers = {
                 "Accept": "audio/wav",
-                "Authorization": f"Bearer {access_token}",
+                "Authorization": f"Bearer {ACCESS_TOKEN}",
                 "Content-Type": "application/json"
             }
             print('________header_________', headers)
