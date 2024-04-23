8e7069ca7f5282e6010166b16569efa196827b67
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-04-23 15:03:56 +0500

    Update react_2.py

--- app/routers/react_2.py
+++ app/routers/react_2.py
@@ -239,36 +239,36 @@ async def get_data():
 # ----------------This endpoint sends requests (using requests library in series manner) to the TTM endpoint and returns the response to the client.
 @router.post("/api/ttm_endpoint")
 async def text_to_music(request: Request, authorization: Optional[str] = Header(TTM_ACCESS_TOKEN)) -> FileResponse:
+    request_data = await request.json()
+    print('_______________request_data_____________', request_data)
+
+    prompt = request_data.get("prompt")
+    print('_______________prompt_____________', prompt)
+
+    duration = request_data.get("duration")
+    print('_______________duration_____________', duration)
+    access_token = authorization
+
+    if prompt is None:
+        raise HTTPException(status_code=400, detail="Prompt is missing in the request body.")
+    
+    if access_token is None:
+        raise HTTPException(status_code=400, detail="Authorization is missing in the request body.")
+
     try:
-        request_data = await request.json()
-        print('_______________request_data_____________', request_data)
+        data = {"prompt": prompt, "duration": duration}
+        headers = {
+            "Accept": "application/json",
+            "Authorization": f"Bearer {access_token}",
+            "Content-Type": "application/json"
+        }
+        print('________header_________', headers)
 
-        prompt = request_data.get("prompt")
-        print('_______________prompt_____________', prompt)
+        # Set the timeout value in seconds (e.g., 30 seconds)
+        # timeout = 500
 
-        duration = request_data.get("duration")
-        print('_______________duration_____________', duration)
-        access_token = authorization
-
-        if prompt is None:
-            raise HTTPException(status_code=400, detail="Prompt is missing in the request body.")
-        
-        if access_token is None:
-            raise HTTPException(status_code=400, detail="Authorization is missing in the request body.")
-
+        print("----------Music generation is in progress. Please wait for a while.----------")
         try:
-            data = {"prompt": prompt, "duration": duration}
-            headers = {
-                "Accept": "application/json",
-                "Authorization": f"Bearer {access_token}",
-                "Content-Type": "application/json"
-            }
-            print('________header_________', headers)
-
-            # Set the timeout value in seconds (e.g., 30 seconds)
-            # timeout = 500
-
-            print("----------Music generation is in progress. Please wait for a while.----------")
             response = requests.post(f"{nginx_url}/api/ttm_endpoint", headers=headers, json=data,
                 # timeout=timeout  # Add the timeout parameter here
                 )
@@ -282,12 +282,11 @@ async def text_to_music(request: Request, authorization: Optional[str] = Header(
                 return FileResponse(temp_file_path, media_type="audio/wav", filename="generated_ttm_audio.wav")
             else:
                 raise HTTPException(status_code=404, detail="--------------Audio file not found---------------")
-            
         except Timeout:
             raise HTTPException(status_code=504, detail="-------------Gateway Timeout: The server timed out waiting for the request----------")
-
-    except ValueError:
-        raise HTTPException(status_code=400, detail="----------------Request not redirected to API no 1 due to invalid routing----------------")
+        
+    except Timeout:
+        raise HTTPException(status_code=504, detail="-------------Gateway Timeout: The server timed out waiting for the request----------")
 
 
 
