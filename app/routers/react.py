6c1561e80845d1d5914b4efb4a2b0ae765538ce1
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-04-29 12:50:59 +0500

    Update react.py

--- app/routers/react.py
+++ app/routers/react.py
@@ -463,31 +463,32 @@ async def verify_email(request : Request, db: Session = Depends(get_database)):
 
         # Find the user by email and mark as verified
         user = db.query(Email_User).filter(Email_User.email == email).first()
-        try:
-            if user:
-                # Check if the verification code is correct
-                if user.password_reset_code != verification_code:
-                    raise HTTPException(status_code=400, detail="Verification code is incorrect")
-                if user.verification_token != token:
-                    raise HTTPException(status_code=400, detail="Verification token is Invalid")
-                
-                # Check if the access token is valid (not expired)
-                exp_timestamp = decoded_token["exp"]
-                exp_datetime_utc = datetime.fromtimestamp(exp_timestamp, tz=timezone.utc)
-                if datetime.now(timezone.utc) >= exp_datetime_utc:
-                    raise jwt.ExpiredSignatureError(status_code=400, detail="Password reset token has expired. Send Forgot Password request again.")
-                
-                user.email_status = "Verified"
-                user.password_reset_code = None
-                user.password_reset_code = None
-                db.commit()
-                db.refresh(user)
-                return {"message": "Email verified successfully"}
-            else:
-                raise HTTPException(status_code=400, detail="User does not exist")
-        except Exception as e:
-            raise HTTPException(status_code=400, detail= str(e))
-
+        if user:
+            # Check if the verification code is correct
+            if user.password_reset_code != verification_code:
+                raise HTTPException(status_code=400, detail="Verification code is incorrect")
+            if user.verification_token != token:
+                raise HTTPException(status_code=400, detail="Verification token is Invalid")
+            
+            # Check if the access token is valid (not expired)
+            exp_timestamp = decoded_token["exp"]
+            exp_datetime_utc = datetime.fromtimestamp(exp_timestamp, tz=timezone.utc)
+            if datetime.now(timezone.utc) >= exp_datetime_utc:
+                raise HTTPException(status_code=400, detail="Password reset token has expired. Send Forgot Password request again.")
+            
+            user.email_status = "Verified"
+            user.password_reset_code = None
+            user.password_reset_code = None
+            db.commit()
+            db.refresh(user)
+            return {"message": "Email verified successfully"}
+        else:
+            raise HTTPException(status_code=400, detail="User does not exist")
+        
+    except jwt.JWTError:
+        raise HTTPException(status_code=400, detail="Invalid token")
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=400, detail="Token has expired")
     except Exception as e:
         raise HTTPException(status_code=400, detail= str(e))
 
