de1e3107e639049cf5062df1a13c574b2df292af
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-04-29 13:45:21 +0500

    Update react.py

--- app/routers/react.py
+++ app/routers/react.py
@@ -519,9 +519,9 @@ async def email_signin(request: Request, db: Session = Depends(get_database)):
                 print("OooPS...Incorrect password. Please try again")
                 raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="OooPS...Incorrect password. Please try again")
             
-            elif email_user.email_status == "unverified":
-                print("OooPS...You have not verified yet. Please verify your email first...")
-                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="OooPS...You have not verified yet. Please verify your email first...")
+            # elif email_user.email_status == "unverified":
+            #     print("OooPS...You have not verified yet. Please verify your email first...")
+            #     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="OooPS...You have not verified yet. Please verify your email first...")
             
             else:
                 # Generate an access token for the user
