74529c9924e6c8431156c982e282c5b7fece3a24
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-04-29 11:22:27 +0500

    Update Email_Verification.py

--- app/routers/Email_Verification.py
+++ app/routers/Email_Verification.py
@@ -192,11 +192,11 @@ def send_verification_email(recipient_email, verification_token, verification_co
 
     <body style="font-family: Arial, sans-serif; background-color: #f5f5f5; padding: 20px;">
 
-        <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 10px; padding: 20px; box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);">
-            <h2 style="color: #333333; text-align: center;">Email Verification</h2>
+        <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 10px; padding: 20px; box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1); text-align: center;">
+            <h2 style="color: #333333;">Email Verification</h2>
             <p style="color: #333333; font-size: 16px; line-height: 1.6;">Dear User,</p>
             <p style="color: #333333; font-size: 16px; line-height: 1.6;">Thank you for signing up with us. Your email verification code is: <strong>{verification_code}</strong>. To complete your registration and verify your email address, please click the button below and enter your email verification code:</p>
-            <div style="text-align: center; margin-top: 20px;">
+            <div style="margin-top: 20px;">
                 <a href="{verification_link}" style="background-color: #007bff; color: #ffffff; text-decoration: none; padding: 10px 20px; border-radius: 5px; font-weight: bold; display: inline-block;">Verify Email</a>
             </div>
             <p style="color: #333333; font-size: 16px; line-height: 1.6; margin-top: 20px;">Thank you,<br><strong>Team Bittaudio.ai</strong></p>
